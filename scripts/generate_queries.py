20465b191fc169b786ecc97962fcf82efb641002
Anurag Koul <koulanurag@gmail.com>
2021-08-06 11:53:50 -0700

    added horizon and gamma info to queries

--- scripts/generate_queries.py
+++ scripts/generate_queries.py
@@ -30,6 +30,10 @@ if __name__ == '__main__':
             observations_b = []
             actions_a = []
             actions_b = []
+            gamma_a = []
+            gamma_b = []
+            horizon_a = []
+            horizon_b = []
             targets_a = []
             targets_b = []
             targets = []
@@ -43,37 +47,49 @@ if __name__ == '__main__':
                 policy_b, _ = policybazaar.get_policy(args.env_name, id_b)
 
                 info = {}
-                # evaluate policy
-                for policy, root_action, name in [(policy_a, root_action_a, 'policy_a'),
-                                                  (policy_b, root_action_b, 'policy_b')]:
-                    episode_rewards = 0
-                    for _ in range(args.max_episodes):
-                        episode_reward = 0
-                        env.seed(seed)
-                        env.reset()
-                        step_obs, step_reward, done, _ = env.step(root_action)
-                        episode_reward += step_reward
-                        while not done:
-                            action = policy.actor(torch.tensor(step_obs).unsqueeze(0).float())
-                            action = action.data.cpu().numpy()[0]
-                            step_obs, step_reward, done, _ = env.step(action)
-                            episode_reward += args.gamma * step_reward
-                        episode_rewards += episode_reward
 
-                    target = episode_rewards / args.max_episodes
-                    info[name] = target
+                for horizon in [10,50,100]:
+                    # evaluate policy
+                    for policy, root_action, name in [(policy_a, root_action_a, 'policy_a'),
+                                                      (policy_b, root_action_b, 'policy_b')]:
+                        episode_rewards = 0
+                        for _ in range(args.max_episodes):
+                            episode_reward = 0
+                            env.seed(seed)
+                            env.reset()
+                            step_obs, step_reward, done, _ = env.step(root_action)
+                            episode_reward += step_reward
+                            for step_count in range(horizon):
+                                action = policy.actor(torch.tensor(step_obs).unsqueeze(0).float())
+                                action = action.data.cpu().numpy()[0]
+                                step_obs, step_reward, done, _ = env.step(action)
+                                episode_reward += args.gamma * step_reward
+                            episode_rewards += episode_reward
 
-                # store data
-                observations_a.append(obs)
-                observations_b.append(obs)
-                actions_a.append(root_action_a)
-                actions_b.append(root_action_b)
-                targets_a.append(info['policy_a'])
-                targets_b.append(info['policy_b'])
-                targets.append(targets_a[-1] < targets_b[-1])
+                        target = episode_rewards / args.max_episodes
+                        info[name] = target
+
+                    # store data
+                    observations_a.append(obs)
+                    observations_b.append(obs)
+
+                    actions_a.append(root_action_a)
+                    actions_b.append(root_action_b)
+
+                    gamma_a.append(args.gamma)
+                    gamma_b.append(args.gamma)
+
+                    horizon_a.append(horizon)
+                    horizon_b.append(horizon)
+
+                    targets_a.append(info['policy_a'])
+                    targets_b.append(info['policy_b'])
+                    targets.append(targets_a[-1] < targets_b[-1])
 
             _key = ((args.env_name, id_a), (args.env_name, id_b))
-            queries[_key] = (observations_a, actions_a, observations_b, actions_b, targets_a, targets_b, targets)
+            queries[_key] = (observations_a, actions_a, horizon_a, gamma_a,
+                             observations_b, actions_b, horizon_b, gamma_b,
+                             targets_a, targets_b, targets)
 
     _path = os.path.join(CQUE_DIR, args.env_name, 'queries.p')
     os.makedirs(os.path.join(CQUE_DIR, args.env_name), exist_ok=True)
