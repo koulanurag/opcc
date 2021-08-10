b6084ce8d0a5a86bb98344f05346e185062b47cc
Anurag Koul <koulanurag@gmail.com>
2021-08-10 13:22:50 -0700

    added horizon and gamma info to queries

--- cque/__init__.py
+++ cque/__init__.py
@@ -5,13 +5,13 @@ import os
 import pickle
 
 
-def measure_uncertainty(prediction, confidence_score, target, confidence_threshold=0.5):
+def measure_uncertainty(prediction, uncertainty_score, target, uncertainty_threshold):
     match = np.zeros(prediction.shape)
-    idk = np.zeros(prediction.shape)
-    confident_idx = confidence_score <= confidence_threshold
-    match[confident_idx] = (prediction[confident_idx] == target[confident_idx]).astype(int)
-    idk[~confident_idx] = 1
-    return match.mean() if sum(match) != 0 else 1, idk.mean(), {}
+    abstain = np.zeros(prediction.shape)
+    certain_idx = uncertainty_score <= uncertainty_threshold
+    match[certain_idx] = (prediction[certain_idx] == target[certain_idx]).astype(int)
+    abstain[~certain_idx] = 1
+    return match.mean() if sum(match) != 0 else 1, abstain.mean(), {}
 
 
 def get_queries(env_name):
