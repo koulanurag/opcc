639e60c46269ff87f022ab562a42fd5e659b10e7
Anurag Koul <koulanurag@gmail.com>
2021-08-11 15:10:13 -0700

    added horizon and gamma info to queries

--- cque/config.py
+++ cque/config.py
@@ -6,13 +6,13 @@ CQUE_DIR = os.getenv('CQUE_DIR', default=os.path.join(str(Path.home()), '.cque')
 MAZE_BASE_PROJECT_URL = 'koulanurag/cque'
 MUJOCO_BASE_PROJECT_URL = 'koulanurag/cque'
 
-MAZE_ENV_IDS = {'d4rl:maze2d-medium-v1': {'wandb_run_path': 'koulanurag/cque/1go232wj'},
-                'd4rl:maze2d-open-dense-v0': {'wandb_run_path': 'koulanurag/cque/j8svyyfy'},
-                'd4rl:maze2d-large-dense-v1': {'wandb_run_path': 'koulanurag/cque/f8v0novr'},
-                'd4rl:maze2d-open-v0': {'wandb_run_path': 'koulanurag/cque/b9y48h5k'},
-                'd4rl:maze2d-medium-dense-v1': {'wandb_run_path': 'koulanurag/cque/3ke5lwka'},
-                'd4rl:maze2d-large-v1': {'wandb_run_path': 'koulanurag/cque/2tsu38l3'},
-                'd4rl:maze2d-umaze-dense-v1': {'wandb_run_path': 'koulanurag/cque/1pr3nqu0'},
-                'd4rl:maze2d-umaze-v1': {'wandb_run_path': 'koulanurag/cque/v0dih0r9'}}
+MAZE_ENV_IDS = {'d4rl:maze2d-medium-v1': {'wandb_run_path': 'koulanurag/cque/awk23x2d'},
+                'd4rl:maze2d-umaze-v1': {'wandb_run_path': 'koulanurag/cque/3vv1qbbu'},
+                'd4rl:maze2d-umaze-dense-v1': {'wandb_run_path': 'koulanurag/cque/3tr3yqx8'},
+                'd4rl:maze2d-large-v1': {'wandb_run_path': 'koulanurag/cque/3oqi1usx'},
+                'd4rl:maze2d-open-dense-v0': {'wandb_run_path': 'koulanurag/cque/30i0aoyh'},
+                'd4rl:maze2d-medium-dense-v1': {'wandb_run_path': 'koulanurag/cque/2nxd6dzp'},
+                'd4rl:maze2d-open-v0': {'wandb_run_path': 'koulanurag/cque/2akwjbh0'},
+                'd4rl:maze2d-large-dense-v1': {'wandb_run_path': 'koulanurag/cque/1lh1snup'}}
 
 ENV_IDS = {**MAZE_ENV_IDS}
--- scripts/generate_queries.py
+++ scripts/generate_queries.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate queries')
     parser.add_argument('--env-name', required=False, default='d4rl:maze2d-open-v0')
     parser.add_argument('--max-episodes', type=int, default=2, required=False)
-    parser.add_argument('--gamma', type=float, default=0.99, required=False)
+    parser.add_argument('--gamma', type=float, default=1, required=False)
 
     # Process arguments
     args = parser.parse_args()
@@ -44,7 +44,7 @@ if __name__ == '__main__':
             targets_b = []
             targets = []
 
-            for seed in range(5):
+            for seed in range(3):
                 env.seed(seed)
                 obs = env.reset().tolist()
                 root_action_a = env.action_space.sample().tolist()
@@ -54,28 +54,26 @@ if __name__ == '__main__':
 
                 info = {}
 
-                for horizon in [10, 50, 100]:
-                    # evaluate policy
-                    for policy, root_action, name in [(policy_a, root_action_a, 'policy_a'),
-                                                      (policy_b, root_action_b, 'policy_b')]:
-                        episode_rewards = 0
-                        for _ in range(args.max_episodes):
-                            episode_reward = 0
-                            env.seed(seed)
-                            step_obs = env.reset()
-                            assert (obs == step_obs).all()
-                            step_obs, step_reward, done, _ = env.step(root_action)
-                            episode_reward += step_reward
-                            for step_count in range(horizon):
-                                action = policy.actor(torch.tensor(step_obs).unsqueeze(0).float())
-                                action = action.data.cpu().numpy()[0]
-                                step_obs, step_reward, done, _ = env.step(action)
-                                episode_reward += args.gamma * step_reward
-                            episode_rewards += episode_reward
+                # evaluate policy
+                horizons = [10, 50, 100]
+                for policy, root_action, name in [(policy_a, root_action_a, 'policy_a'),
+                                                  (policy_b, root_action_b, 'policy_b')]:
+                    max_horizon = max(horizons)
+                    episode_rewards = np.zeros((2, max_horizon))
+                    for ep_i in range(args.max_episodes):
+                        env.seed(seed)
+                        step_obs = env.reset()
+                        assert (obs == step_obs).all()
+                        step_obs, step_reward, done, _ = env.step(root_action)
+                        episode_rewards[ep_i, 0] = step_reward
+                        for step_count in range(1, max_horizon):
+                            action = policy.actor(torch.tensor(step_obs).unsqueeze(0).float())
+                            action = action.data.cpu().numpy()[0]
+                            step_obs, step_reward, done, _ = env.step(action)
+                            episode_rewards[ep_i, step_count] = step_reward
+                    info[name] = episode_rewards
 
-                        target = episode_rewards / args.max_episodes
-                        info[name] = target
-
+                for horizon in horizons:
                     # store data
                     observations_a.append(obs)
                     observations_b.append(obs)
@@ -89,8 +87,8 @@ if __name__ == '__main__':
                     horizon_a.append(horizon)
                     horizon_b.append(horizon)
 
-                    targets_a.append(info['policy_a'])
-                    targets_b.append(info['policy_b'])
+                    targets_a.append(info['policy_a'][:, :horizon].sum(1).mean())
+                    targets_b.append(info['policy_b'][:, :horizon].sum(1).mean())
                     targets.append(targets_a[-1] < targets_b[-1])
 
             _key = ((args.env_name, id_a), (args.env_name, id_b))
@@ -107,7 +105,7 @@ if __name__ == '__main__':
 
     df = pd.DataFrame(
         data={'q-value-a': overall_target_a, 'q-value-b': overall_target_b, 'target': overall_target,
-              'horizon':overall_horizon})
+              'horizon': overall_horizon})
     fig = px.scatter_3d(df, x='q-value-a', y='q-value-b', z='target', color='target', symbol='horizon')
     wandb.log({'query-values': fig})
     _path = os.path.join(CQUE_DIR, args.env_name, 'queries.p')
