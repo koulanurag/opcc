43c94562ef3c5844b9f46ec0d8412d48e228ca58
Anurag Koul <koulanurag@gmail.com>
2021-08-17 10:00:18 -0700

    added horizon and gamma info to queries

--- cque/config.py
+++ cque/config.py
@@ -6,13 +6,13 @@ CQUE_DIR = os.getenv('CQUE_DIR', default=os.path.join(str(Path.home()), '.cque')
 MAZE_BASE_PROJECT_URL = 'koulanurag/cque'
 MUJOCO_BASE_PROJECT_URL = 'koulanurag/cque'
 
-MAZE_ENV_IDS = {'d4rl:maze2d-large-v1': {'wandb_run_path': 'koulanurag/cque/2jpytkj9'},
-                'd4rl:maze2d-medium-dense-v1': {'wandb_run_path': 'koulanurag/cque/21f6capa'},
-                'd4rl:maze2d-medium-v1': {'wandb_run_path': 'koulanurag/cque/57zmcmti'},
-                'd4rl:maze2d-umaze-dense-v1': {'wandb_run_path': 'koulanurag/cque/3oz7iz95'},
-                'd4rl:maze2d-large-dense-v1': {'wandb_run_path': 'koulanurag/cque/2sa8v52m'},
-                'd4rl:maze2d-open-v0': {'wandb_run_path': 'koulanurag/cque/2ryjyty0'},
-                'd4rl:maze2d-umaze-v1': {'wandb_run_path': 'koulanurag/cque/2cg2at9k'},
-                'd4rl:maze2d-open-dense-v0': {'wandb_run_path': 'koulanurag/cque/1vb4o2fl'}}
+MAZE_ENV_IDS = {'d4rl:maze2d-umaze-dense-v1': {'wandb_run_path': 'koulanurag/cque/a72gmlma'},
+                'd4rl:maze2d-open-dense-v0': {'wandb_run_path': 'koulanurag/cque/2mg1cey3'},
+                'd4rl:maze2d-large-v1': {'wandb_run_path': 'koulanurag/cque/2dy4i23z'},
+                'd4rl:maze2d-umaze-v1': {'wandb_run_path': 'koulanurag/cque/1gvdzev8'},
+                'd4rl:maze2d-medium-v1': {'wandb_run_path': 'koulanurag/cque/fh34w5xq'},
+                'd4rl:maze2d-open-v0': {'wandb_run_path': 'koulanurag/cque/2fw51oua'},
+                'd4rl:maze2d-medium-dense-v1': {'wandb_run_path': 'koulanurag/cque/21qelw3l'},
+                'd4rl:maze2d-large-dense-v1': {'wandb_run_path': 'koulanurag/cque/1ziou54w'}}
 
 ENV_IDS = {**MAZE_ENV_IDS}
