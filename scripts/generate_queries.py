32c4aa6baa47c79ff63956e31802fc8b343407e6
Anurag Koul <koulanurag@gmail.com>
2021-10-01 16:11:33 -0700

    fix: query gen. | wip

--- scripts/generate_queries.py
+++ scripts/generate_queries.py
@@ -11,32 +11,74 @@ import gym
 import torch
 import plotly.express as px
 import pandas as pd
+from copy import deepcopy
+
+
+def mc_return(env, init_action, horizon, policy, max_episodes):
+    expected_score = 0
+    for ep_i in range(max_episodes):
+
+        _env = deepcopy(env)
+        obs, reward, done, info = _env.step(init_action)
+        score = reward
+        step_count = 1
+
+        while not done and step_count < horizon:
+            action = policy.actor(torch.tensor(obs).unsqueeze(0).float())
+            obs, reward, done, info = _env.step(action.data.cpu().numpy()[0])
+
+            if 'TimeLimit.truncated' in info and info['TimeLimit.truncated']:
+                done = False
+            step_count += 1
+            score += reward
+
+        expected_score += score
+
+    return expected_score / max_episodes
+
 
 if __name__ == '__main__':
     # Lets gather arguments
     parser = argparse.ArgumentParser(description='Generate queries')
     parser.add_argument('--env-name', required=False, default='d4rl:maze2d-open-v0')
-    parser.add_argument('--max-episodes', type=int, default=2, required=False)
-    parser.add_argument('--gamma', type=float, default=1, required=False)
+    parser.add_argument('--max-eval-episodes', type=int, default=2, required=False)
+    parser.add_argument('--noise', type=float, default=0.05, required=False)
     parser.add_argument('--episode-count', type=float, default=5, required=False)
     parser.add_argument('--horizons', '--list', nargs='+', help='<Required> Set flag', type=int, required=True)
+    parser.add_argument('--use-wandb', action='store_true', default=False)
 
     # Process arguments
     args = parser.parse_args()
-    wandb.init(project='cque', config={'env-name': args.env_name})
-
-    queries = {}
-    overall_target_a = []
-    overall_target_b = []
-    overall_target = []
-    overall_horizon = []
-    env = gym.make(args.env_name)
+    if args.use_wandb:
+        wandb.init(project='cque', config={'env-name': args.env_name})
 
     np.random.seed(0)
     random.seed(0)
 
+    env_states = []
+    env = gym.make(args.env_name)
+    while len(env_states) < 1000:
+        policy_id = random.randint(1, 4)
+        model, model_info = policybazaar.get_policy(args.env_name, policy_id)
+        obs = env.reset()
+
+        done = False
+        while not done:
+            save = random.random() >= 0.7
+            if save:
+                env_states.append((obs, deepcopy(env)))
+            action = model.actor(torch.tensor(obs).unsqueeze(0).float())
+            noise = torch.normal(0, args.noise, size=action.shape)
+            step_action = (action + noise).data.cpu().numpy()[0]
+            obs, _, done, info = env.step(step_action)
+
+    # evaluate queries
+    overall_data = {'q-value-a': [], 'q-value-b': [], 'target': [], 'horizon-a': [], 'horizon-b': []}
+    queries = {}
     for id_a in range(1, 5):
+        policy_a, _ = policybazaar.get_policy(args.env_name, id_a)
         for id_b in range(1, 5):
+            policy_b, _ = policybazaar.get_policy(args.env_name, id_b)
 
             states_a = []
             states_b = []
@@ -44,6 +86,9 @@ if __name__ == '__main__':
             actions_b = []
             horizons_a = []
             horizons_b = []
+            targets_a = []
+            targets_b = []
+            targets = []
 
             for query_count in range(50):
                 same_state = random.choice([True, False])
@@ -54,126 +99,64 @@ if __name__ == '__main__':
                 else:
                     same_horizon = random.choice([True, False])
 
-                sample_from_dataset = random.choice([True, False])
-                if sample_from_dataset:
-                    state_a, action_a = x
-                    if same_state:
-                        state_b, action_b = state_a, action_a
-                    else:
-                        state_b, action_b = sample
+                state_a = random.choice(env_states)
+                action_a = env.action_space.sample()
+
+                _state_b, _action_b = random.choice(env_states), env.action_space.sample()
+                if same_state:
+                    state_b = state_a
                 else:
-                    state_a = x
-                    action_a = x
+                    state_b = _state_b
+
+                if same_action:
+                    action_b = action_a
+                else:
+                    action_b = _action_b
 
                 if same_horizon:
-                    _horizon = random.choice(horizon_set)
+                    _horizon = random.choice(args.horizons)
                     horizon_a, horizon_b = _horizon, _horizon
                 else:
-                    horizon_a, horizon_b = random.choice(horizon_set), random.choice(horizon_set)
+                    horizon_a, horizon_b = random.choice(args.horizons), random.choice(args.horizons)
 
-            # evaluate
-            targets_a, targets_b, targets = [], [], []
-            for ep_i in range(args.max_episodes):
-                env.seed(seed)
-                step_obs = env.reset()
-                assert (obs == step_obs).all()
-                step_obs, step_reward, done, _ = env.step(np.array(root_action))
-                episode_rewards[ep_i, 0] = step_reward
-                for step_count in range(1, max_horizon):
-                    action = policy.actor(torch.tensor(step_obs).unsqueeze(0).float())
-                    action = action.data.cpu().numpy()[0]
-                    step_obs, step_reward, done, _ = env.step(action)
-                    episode_rewards[ep_i, step_count] = step_reward
+                # evaluate
+                target_a = mc_return(env, action_a, horizon_a, policy_a, args.max_eval_episodes)
+                target_b = mc_return(env, action_b, horizon_b, policy_b, args.max_eval_episodes)
+
+                states_a.append(state_a)
+                states_b.append(state_b)
+                actions_a.append(action_a)
+                actions_b.append(action_b)
+                horizons_a.append(horizon_a)
+                horizons_b.append(horizon_b)
+                targets_a.append(target_a)
+                targets_b.append(target_b)
+                targets.append(target_a < target_b)
 
             _key = ((args.env_name, id_a), (args.env_name, id_b))
             queries[_key] = (states_a, actions_a, horizons_a,
                              states_b, actions_b, horizons_b,
                              targets_a, targets_b, targets)
 
-    for id_a in range(1, 4):
-        for id_b in range(1, 4):
-            if id_a == id_b:
-                continue
+            # save data separately for ease of visualization
+            overall_data['q-value-a'] += targets_a
+            overall_data['q-value-b'] += targets_b
+            overall_data['target'] += targets
+            overall_data['horizon-a'] += horizons_a
+            overall_data['horizon-b'] += horizons_b
 
-            observations_a = []
-            observations_b = []
-            actions_a = []
-            actions_b = []
-            gamma_a = []
-            gamma_b = []
-            horizon_a = []
-            horizon_b = []
-            targets_a = []
-            targets_b = []
-            targets = []
+    # visualize
+    if args.use_wandb:
+        df = pd.DataFrame(data=overall_data)
+        fig2 = px.scatter(df, x='q-value-a', y='q-value-b', color='target')
+        wandb.log({'query-values-scatter': fig2})
 
-            for seed in range(args.episode_count):
-                env.seed(seed)
-                obs = env.reset().tolist()
-                root_action_a = env.action_space.sample().tolist()
-                root_action_b = env.action_space.sample().tolist()
-                policy_a, _ = policybazaar.get_policy(args.env_name, id_a)
-                policy_b, _ = policybazaar.get_policy(args.env_name, id_b)
-
-                info = {}
-
-                # evaluate policy
-                horizons = args.horizons
-                for policy, root_action, name in [(policy_a, root_action_a, 'policy_a'),
-                                                  (policy_b, root_action_b, 'policy_b')]:
-                    max_horizon = max(horizons)
-                    episode_rewards = np.zeros((2, max_horizon))
-                    for ep_i in range(args.max_episodes):
-                        env.seed(seed)
-                        step_obs = env.reset()
-                        assert (obs == step_obs).all()
-                        step_obs, step_reward, done, _ = env.step(np.array(root_action))
-                        episode_rewards[ep_i, 0] = step_reward
-                        for step_count in range(1, max_horizon):
-                            action = policy.actor(torch.tensor(step_obs).unsqueeze(0).float())
-                            action = action.data.cpu().numpy()[0]
-                            step_obs, step_reward, done, _ = env.step(action)
-                            episode_rewards[ep_i, step_count] = step_reward
-                    info[name] = episode_rewards
-
-                for horizon in horizons:
-                    # store data
-                    observations_a.append(obs)
-                    observations_b.append(obs)
-
-                    actions_a.append(root_action_a)
-                    actions_b.append(root_action_b)
-
-                    gamma_a.append(args.gamma)
-                    gamma_b.append(args.gamma)
-
-                    horizon_a.append(horizon)
-                    horizon_b.append(horizon)
-
-                    targets_a.append(info['policy_a'][:, :horizon].sum(1).mean())
-                    targets_b.append(info['policy_b'][:, :horizon].sum(1).mean())
-                    targets.append(targets_a[-1] < targets_b[-1])
-
-            _key = ((args.env_name, id_a), (args.env_name, id_b))
-            queries[_key] = (observations_a, actions_a, horizon_a, gamma_a,
-                             observations_b, actions_b, horizon_b, gamma_b,
-                             targets_a, targets_b, targets)
-            overall_target_a += targets_a
-            overall_target_b += targets_b
-            overall_target += targets
-            overall_horizon += horizon_a
-
-    df = pd.DataFrame(data={'q-value-a': overall_target_a,
-                            'q-value-b': overall_target_b,
-                            'target': overall_target,
-                            'horizon': overall_horizon})
-    fig1 = px.scatter_3d(df, x='q-value-a', y='q-value-b', z='target', color='target', symbol='horizon')
-    fig2 = px.scatter(df, x='q-value-a', y='q-value-b', color='target')
-    wandb.log({'query-values-3d': fig1, 'query-values-scatter': fig2})
+    # save queries
     _path = os.path.join(CQUE_DIR, args.env_name, 'queries.p')
     os.makedirs(os.path.join(CQUE_DIR, args.env_name), exist_ok=True)
     pickle.dump(queries, open(_path, 'wb'))
-    wandb.save(_path)
+    if args.use_wandb:
+        wandb.save(_path)
 
     # close env
     env.close()
