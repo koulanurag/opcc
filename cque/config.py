bc645e5ea703aad6e2b6f20e9bfa761658985a1b
Anurag Koul <koulanurag@gmail.com>
2022-01-03 06:18:15 -0800

    fix: updated config run-ids

--- cque/config.py
+++ cque/config.py
@@ -6,24 +6,24 @@ CQUE_DIR = os.getenv('CQUE_DIR', default=os.path.join(str(Path.home()), '.cque')
 MAZE_BASE_PROJECT_URL = 'koulanurag/cque'
 MUJOCO_BASE_PROJECT_URL = 'koulanurag/cque'
 
-MAZE_ENV_IDS = {'d4rl:maze2d-umaze-v1': {'wandb_run_path': 'koulanurag/cque/67h9jtdt',
+MAZE_ENV_IDS = {'d4rl:maze2d-umaze-v1': {'wandb_run_path': 'koulanurag/cque/33bhoifl',
                                          'datasets': {'1k': {'name': 'd4rl:maze2d-umaze-v1', 'split': 1000},
                                                       '10k': {'name': 'd4rl:maze2d-umaze-v1', 'split': 10000},
                                                       '100k': {'name': 'd4rl:maze2d-umaze-v1', 'split': 100000}}},
-                'd4rl:maze2d-medium-v1': {'wandb_run_path': 'koulanurag/cque/2a89clfg',
+                'd4rl:maze2d-medium-v1': {'wandb_run_path': 'koulanurag/cque/2osvzocb',
                                           'datasets': {'1k': {'name': 'd4rl:maze2d-medium-v1', 'split': 1000},
                                                        '10k': {'name': 'd4rl:maze2d-medium-v1', 'split': 10000},
                                                        '100k': {'name': 'd4rl:maze2d-medium-v1', 'split': 100000}}},
-                'd4rl:maze2d-open-v0': {'wandb_run_path': 'koulanurag/cque/2jafc58a',
+                'd4rl:maze2d-open-v0': {'wandb_run_path': 'koulanurag/cque/1fj9b8n7',
                                         'datasets': {'1k': {'name': 'd4rl:maze2d-open-v0', 'split': 1000},
                                                      '10k': {'name': 'd4rl:maze2d-open-v0', 'split': 10000},
                                                      '100k': {'name': 'd4rl:maze2d-open-v0', 'split': 100000}}},
-                'd4rl:maze2d-large-v1': {'wandb_run_path': 'koulanurag/cque/2ruj7m4x',
+                'd4rl:maze2d-large-v1': {'wandb_run_path': 'koulanurag/cque/119su6ff',
                                          'datasets': {'1k': {'name': 'd4rl:maze2d-large-v1', 'split': 1000},
                                                       '10k': {'name': 'd4rl:maze2d-large-v1', 'split': 10000},
                                                       '100k': {'name': 'd4rl:maze2d-large-v1', 'split': 100000}}}}
 
-MUJOCO_ENV_IDS = {'HalfCheetah-v2': {'wandb_run_path': 'koulanurag/cque/3vs9ct3b',
+MUJOCO_ENV_IDS = {'HalfCheetah-v2': {'wandb_run_path': 'koulanurag/cque/n3thim8m',
                                      'datasets': {'random': {'name': 'd4rl:halfcheetah-random-v2', 'split': None},
                                                   'expert': {'name': 'd4rl:halfcheetah-expert-v2', 'split': None},
                                                   'medium': {'name': 'd4rl:halfcheetah-medium-v2', 'split': None},
@@ -31,19 +31,19 @@ MUJOCO_ENV_IDS = {'HalfCheetah-v2': {'wandb_run_path': 'koulanurag/cque/3vs9ct3b
                                                                     'split': None},
                                                   'medium-expert': {'name': 'd4rl:halfcheetah-medium-expert-v2',
                                                                     'split': None}}},
-                  'Walker2d-v2': {'wandb_run_path': 'koulanurag/cque/2p1gpg7l',
+                  'Hopper-v2': {'wandb_run_path': 'koulanurag/cque/387mvs7s',
+                                'datasets': {'random': {'name': 'd4rl:hopper-random-v2', 'split': None},
+                                             'expert': {'name': 'd4rl:hopper-expert-v2', 'split': None},
+                                             'medium': {'name': 'd4rl:hopper-medium-v2', 'split': None},
+                                             'medium-replay': {'name': 'd4rl:hopper-medium-replay-v2', 'split': None},
+                                             'medium-expert': {'name': 'd4rl:hopper-medium-expert-v2', 'split': None}}},
+                  'Walker2d-v2': {'wandb_run_path': 'koulanurag/cque/29ntrhnd',
                                   'datasets': {'random': {'name': 'd4rl:walker2d-random-v2', 'split': None},
                                                'expert': {'name': 'd4rl:walker2d-expert-v2', 'split': None},
                                                'medium': {'name': 'd4rl:walker2d-medium-v2', 'split': None},
                                                'medium-replay': {'name': 'd4rl:walker2d-medium-replay-v2',
                                                                  'split': None},
                                                'medium-expert': {'name': 'd4rl:walker2d-medium-expert-v2',
-                                                                 'split': None}}},
-                  'Hopper-v2': {'wandb_run_path': 'koulanurag/cque/2agy42nf',
-                                'datasets': {'random': {'name': 'd4rl:hopper-random-v2', 'split': None},
-                                             'expert': {'name': 'd4rl:hopper-expert-v2', 'split': None},
-                                             'medium': {'name': 'd4rl:hopper-medium-v2', 'split': None},
-                                             'medium-replay': {'name': 'd4rl:hopper-medium-replay-v2', 'split': None},
-                                             'medium-expert': {'name': 'd4rl:hopper-medium-expert-v2', 'split': None}}}}
+                                                                 'split': None}}}}
 
 ENV_IDS = {**MAZE_ENV_IDS, **MUJOCO_ENV_IDS}
