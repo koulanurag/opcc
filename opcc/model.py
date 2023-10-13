d1980c70cf320018c49051f8ddaf5d6d0ca93e22
koulanurag <koulanurag@gmail.com>
2023-10-13 15:46:19 -0400

    fix(policy): remove redundant funcs.

--- opcc/model.py
+++ opcc/model.py
@@ -31,7 +31,7 @@ class ValueNetwork(nn.Module):
 
 
 class ActorNetwork(nn.Module):
-    def __init__(self, state_dim, num_actions, hidden_dim, action_std):
+    def __init__(self, state_dim, num_actions, hidden_dim, max_action=1):
         super(ActorNetwork, self).__init__()
 
         self.num_actions = num_actions
@@ -40,31 +40,20 @@ class ActorNetwork(nn.Module):
         self.linear3 = nn.Linear(hidden_dim, num_actions)
 
         self.apply(weights_init_)
-        self.action_var = torch.full((num_actions,), action_std * action_std)
+        self.max_action = max_action
 
     def forward(self, state):
         x = torch.tanh(self.linear1(state))
         x = torch.tanh(self.linear2(x))
         action_mean = torch.tanh(self.linear3(x))
 
-        return action_mean
-
-    def dist(self, state):
-        action_mean = self.forward(state)
-        cov_mat = torch.diag(self.action_var)
-        dist = MultivariateNormal(action_mean, cov_mat)
-        return dist
-
-    def to(self, device):
-        self.action_var = self.action_var.to(device)
-        return super(ActorNetwork, self).to(device)
+        return self.max_action * action_mean
 
 
 class ActorCriticNetwork(nn.Module):
-    def __init__(self, num_inputs, num_actions, hidden_dim, action_std):
+    def __init__(self, num_inputs, num_actions, hidden_dim):
         super(ActorCriticNetwork, self).__init__()
-        self.actor = ActorNetwork(num_inputs, num_actions, hidden_dim,
-                                  action_std)
+        self.actor = ActorNetwork(num_inputs, num_actions, hidden_dim)
         self.critic = ValueNetwork(num_inputs, hidden_dim)
 
     def to(self, device):
