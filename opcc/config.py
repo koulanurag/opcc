f516bc7a396dbc503bc162af393d46db7b24b603
koulanurag <koulanurag@gmail.com>
2023-10-16 16:07:48 -0400

    feat: added door-v0 and pen-v0 stats

--- opcc/config.py
+++ opcc/config.py
@@ -181,15 +181,21 @@ ENV_PERFORMANCE_STATS = {
         4: {"score_mean": 162.7, "score_std": 102.14},
     },
     "door-v0": {
-        1: {"score_mean": 3011.04, "score_std": 380.93},
+        1: {"score_mean": 2924.69, "score_std": 921.62},
         2: {"score_mean": 2396.11, "score_std": 538.26},
         3: {"score_mean": 1632.13, "score_std": 790.67},
         4: {"score_mean": -58.07, "score_std": 0.58},
     },
+    "pen-v0": {
+        1: {"score_mean": 3882.5, "score_std": 1896.45},
+        2: {"score_mean": 3105.94, "score_std": 1971.26},
+        3: {"score_mean": 1394.94, "score_std": 1608.43},
+        4: {"score_mean": 248.85, "score_std": 694.86},
+    },
 }
 
 ADROIT_ENV_CONFIGS = {
-    "door-v0": {
+    "d4rl:door-v0": {
         "actor_kwargs": {
             "state_dim": 39,
             "action_dim": 28,
@@ -201,7 +207,20 @@ ADROIT_ENV_CONFIGS = {
             "cloned": {"name": "d4rl:door-cloned-v0", "split": None},
             "expert": {"name": "d4rl:door-expert-v0", "split": None},
         },
-    }
+    },
+    "d4rl:pen-v0": {
+        "actor_kwargs": {
+            "state_dim": 45,
+            "action_dim": 24,
+            "max_action": 1,
+            "hidden_dim": 256,
+        },
+        "datasets": {
+            "human": {"name": "d4rl:pen-human-v0", "split": None},
+            "cloned": {"name": "d4rl:pen-cloned-v0", "split": None},
+            "expert": {"name": "d4rl:pen-expert-v0", "split": None},
+        },
+    },
 }
 
 ENV_CONFIGS = {**MAZE_ENV_CONFIGS, **MUJOCO_ENV_CONFIGS, **ADROIT_ENV_CONFIGS}
