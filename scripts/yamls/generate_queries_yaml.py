031e70c74bf59b48b9ce55e0b958b9fb52bff38e
koulanurag <koulanurag@gmail.com>
2023-10-24 15:02:13 -0400

    docs: init-commit

--- scripts/yamls/generate_queries_yaml.py
+++ scripts/yamls/generate_queries_yaml.py
@@ -68,9 +68,9 @@ ADROIT_ENVS = [
 ENVS = ADROIT_ENVS
 HORIZONS = [10, 20, 30, 40, 50]
 IGNORE_DELTAS_PER_HORIZON = {
-    "d4rl:pen-v0": [700, 1400, 2100, 2800, 3500],
-    "d4rl:hammer-v0": [1000, 2000, 3000, 4000, 5000],
-    "d4rl:door-v0": [200, 400, 600, 800, 1000],
+    "d4rl:pen-v0": [x // 4 for x in [700, 1400, 2100, 2800, 3500]],
+    "d4rl:hammer-v0": [x // 4 for x in [1000, 2000, 3000, 4000, 5000]],
+    "d4rl:door-v0": [x // 4 for x in [200, 400, 600, 800, 1000]],
 }
 
 ENV_VARIABLES = {
@@ -146,7 +146,7 @@ def main():
             f" --horizons {' '.join([str(x) for x in HORIZONS])}"
             " --policy-ids 1 2 3 4"
             " --noise 0.1 "
-            " --eval-runs 10"
+            " --eval-runs 50"
             f" --ignore-delta-per-horizons {' '.join([str(x) for x in IGNORE_DELTAS_PER_HORIZON[env_name]])}"
             " --max-trans-count 100000 "
             "--ignore-stuck-count 5000"
